5705c52b4e25b293052977b2aad6ce8e289fd35c
Max Lawton <max@lawtonnation.com>
2024-06-02 17:09:33 -0400

    no need fro heatmap

--- heatmap.py
+++
@@ -1,7 +0,0 @@
-import geopandas as gpd
-import geodatasets
-import folium
-import matplotlib.pyplot as plt
-
-m = folium.Map(location=[40.70, -73.94], zoom_start=10, tiles="CartoDB positron")
-m.save("show.html")
